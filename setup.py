ecafca4ec95aa4cbbc3e4074590dc8dab3a55c7b
Milkey Mouse <milkeymouse@team-ivan.com>
2016-05-27 00:19:09 -0700

    pep8ify setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ except ImportError:
     ensurepip.bootstrap()
     importlib.invalidate_caches()
     import pip
-          
+
+
 def install_package(pkg):
     print("Installing {}...".format(pkg))
     tmp_stdout = sys.stdout
@@ -36,8 +37,9 @@ def install_package(pkg):
             raise ImportError
         else:
             importlib.invalidate_caches()
-    
+
 from setuptools import setup
+
 
 def get_flags():
     sse4 = False
@@ -45,33 +47,33 @@ def get_flags():
     try:
         # something feels very wrong about running assembly in python
         # most of this taken from https://github.com/workhorsy/py-cpuinfo
-        
-        sixtyfour = sys.maxsize > 2**32  # the docs say platform.machine() can be unreliable
-                
+
+        sixtyfour = sys.maxsize > 2 ** 32  # the docs say platform.machine() can be unreliable
+
         def asm_func(restype=None, argtypes=(), byte_code=[]):
-                byte_code = bytes.join(b'', byte_code)
-                address = None
-                # Allocate a memory segment the size of the byte code
-                size = len(byte_code)
-                address = ctypes.pythonapi.valloc(size)
-                if not address:
-                    raise Exception("Failed to valloc")
-                # Mark the memory segment as writeable only
-                WRITE = 0x2
-                if ctypes.pythonapi.mprotect(address, size, WRITE) < 0:
-                    raise Exception("Failed to mprotect")
-                # Copy the byte code into the memory segment
-                if ctypes.pythonapi.memmove(address, byte_code, size) < 0:
-                    raise Exception("Failed to memmove")
-                # Mark the memory segment as writeable and executable only
-                WRITE_EXECUTE = 0x2 | 0x4
-                if ctypes.pythonapi.mprotect(address, size, WRITE_EXECUTE) < 0:
-                    raise Exception("Failed to mprotect")
-                # Cast the memory segment into a function
-                functype = ctypes.CFUNCTYPE(restype, *argtypes)
-                fun = functype(address)
-                return fun, address
-                
+            byte_code = bytes.join(b'', byte_code)
+            address = None
+            # Allocate a memory segment the size of the byte code
+            size = len(byte_code)
+            address = ctypes.pythonapi.valloc(size)
+            if not address:
+                raise Exception("Failed to valloc")
+            # Mark the memory segment as writeable only
+            WRITE = 0x2
+            if ctypes.pythonapi.mprotect(address, size, WRITE) < 0:
+                raise Exception("Failed to mprotect")
+            # Copy the byte code into the memory segment
+            if ctypes.pythonapi.memmove(address, byte_code, size) < 0:
+                raise Exception("Failed to memmove")
+            # Mark the memory segment as writeable and executable only
+            WRITE_EXECUTE = 0x2 | 0x4
+            if ctypes.pythonapi.mprotect(address, size, WRITE_EXECUTE) < 0:
+                raise Exception("Failed to mprotect")
+            # Cast the memory segment into a function
+            functype = ctypes.CFUNCTYPE(restype, *argtypes)
+            fun = functype(address)
+            return fun, address
+
         def run_asm(*byte_code):
             # Convert the byte code into a function that returns an int
             func, address = asm_func(ctypes.c_uint64 if sixtyfour else ctypes.c_uint32, (), byte_code)
@@ -89,27 +91,27 @@ def get_flags():
         def is_bit_set(reg, bit):
             mask = 1 << bit
             return reg & mask > 0
-            
+
         def eax():
             if sixtyfour:
-                return b"\x66\xB8\x01\x00" # mov eax,0x1"
+                return b"\x66\xB8\x01\x00"  # mov eax,0x1"
             else:
                 return (b"\x31\xC0"         # xor ax,ax
                         b"\x40")            # inc ax
-            
+
         ecx = run_asm(eax(),
                       b"\x0f\xa2"         # cpuid
                       b"\x89\xC8"         # mov ax,cx
                       b"\xC3")            # ret
-                      
+
         ebx = run_asm(
-                b"\xB8\x01\x00\x00\x80" # mov ax,0x80000001
+                b"\xB8\x01\x00\x00\x80"  # mov ax,0x80000001
                 b"\x0f\xa2"         # cpuid
                 b"\x89\xD8"         # mov ax,bx
                 b"\xC3")            # ret
-            
+
         sse4 = is_bit_set(ecx, 19) or is_bit_set(ecx, 20)
-            
+
         avx2 = is_bit_set(ebx, 5)  # because a midi synthesizer definitely needs to support server-grade hardware
     except Exception:
         pass
@@ -117,10 +119,10 @@ def get_flags():
 
 if len(sys.argv) > 1 and "install" in sys.argv:
     pkgs = [package.project_name.lower() for package in pip.get_installed_distributions()]
-    
+
     install = False
     simd = False
-    
+
     if "pillow-simd" in pkgs:
         print("pillow-simd is already installed. swood will install with SIMD support.")
         install = False
@@ -140,15 +142,15 @@ if len(sys.argv) > 1 and "install" in sys.argv:
                 simd = True
         except:
             simd = False
-    
+
     # we need to install numpy first because pyfftw needs it and pip has bad dependency resolution
     # and we need to set this flag because pyFFTW uses a deprecated API apparently
-    os.environ["CFLAGS"] = "-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"  
-    
+    os.environ["CFLAGS"] = "-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"
+
     for pkg in ("numpy", "pyfftw", "progressbar2"):
         if pkg not in pkgs:
             install_package(pkg)
-    
+
     if install:
         try:
             install_package("pillow-simd")
@@ -188,4 +190,3 @@ setup(
         ],
     },
 )
-   
