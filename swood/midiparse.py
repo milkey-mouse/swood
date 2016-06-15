e123364578faf8b60f7e689206b5b4cc7a70a347
Milkey Mouse <milkeymouse@team-ivan.com>
2016-06-15 14:06:04 -0700

    Get sample start time

--- swood/midiparse.py
+++ swood/midiparse.py
@@ -36,10 +36,8 @@ class CachedNote:
 
 
 class MIDIParser:
-    def __init__(self, filename, wav, transpose=0, speed=1):  # TODO: convert the rest of the function to the new notes
-        if speed <= 0:
-            return ValueError("The speed must be a positive number.")
-        results = collections.defaultdict(list)
+    def __init__(self, filename, sample, transpose=0, speed=1):  # TODO: convert the rest of the function to the new notes
+        playing = collections.defaultdict(list)
         notes = collections.defaultdict(list)
         self.notecount = 0
         self.maxvolume = 0
@@ -52,62 +50,60 @@ class MIDIParser:
                 time = 0
                 for message in mid:
                     time += message.time / speed
-                    time_samples = int(round(time * wav.framerate)
                     if "channel" in vars(message) and message.channel == 10:
                         continue  # channel 10 is reserved for percussion
+                    time_samples = int(round(time * sample.framerate)
                     if message.type == "note_on":
                         note = Note()
                         note.starttime = time_samples
                         note.volume = 127 if message.velocity == 0 else message.velocity
-                        note.pitch = message.note + transpose)
+                        note.pitch = message.note + transpose
                         note.bend = bend
-                        notes[message.note].append(note)
+                        playing[message.note].append(note)
 
                         volume += note.volume
                         self.maxvolume = max(volume, self.maxvolume)
                         self.maxpitch = max(self.maxpitch, note.pitch + note.bend)
                     elif message.type == "note_off":
-                        note = notes[message.note].pop(0)
-                        if len(notes[message.note]) == 0:
-                            del notes[message.note]
-
+                        note = playing[message.note].pop(0)
+                        if len(playing[message.note]) == 0:
+                            del playing[message.note]
                         try:
-                            results[note.starttime].append(note)
+                            notes[note.starttime].append(note)
                         except IndexError:
                             print("Warning: There was a note end event at {} seconds with no matching begin event".format(time))
 
                         self.notecount += 1
                         volume -= note.volume
+                        note.pitch = self.note_to_freq(note.pitch + note.bend)
                         note.length = time_samples - note.starttime
+                        del note.bend
                     elif message.type == "pitchwheel":
                         #stop the note and start a new one at that time
                         bend = message.pitches / 8192 * 12
-                        for notelist in notes:
+                        for notelist in playing:
                             for note in notelist:
+                                note.samplestart = note.length * sample.length * self.sample.fundamental_freq / note.pitch
                                 note.length = time_samples - note.starttime
-                                results[note.starttime].append(note)
-                                self.notecount += 1
-                                note.samplestart = note.length * #todo: finish
-                                note.length = None
-                                
-                                
+                                notes[note.starttime].append(note)
                                 note.starttime = time_samples
+                                self.notecount += 1
+                                note.length = None
                                 note.bend = bend
-                                
-                            
-
-                if len(notes) != 0:
+                if len(playing) != 0:
                     print("Warning: The MIDI ended with notes still playing, assuming they end when the MIDI does")
-                    for ntime, nlist in notes.items():
+                    for ntime, nlist in playing.items():
                         for note in nlist:
-                            note.length = int(time * wav.framerate) - note.starttime
+                            note.length = int(time * sample.framerate) - note.starttime
                             self.notecount += 1
-
-                self.notes = sorted(results.items(), key=operator.itemgetter(0))
-                self.length = max(max(note.starttime + note.length for note in nlist) for _, nlist in self.notes)
+                self.playing = sorted(notes.items(), key=operator.itemgetter(0))
+                self.length = max(max(note.starttime + note.length for note in nlist) for _, nlist in self.playing)
+                self.maxpitch = self.note_to_freq(self.maxpitch)
         except IOError:
             raise complain.ComplainToUser("Error opening MIDI file '{}'.".format(filename))
         except IndexError:
-            raise complain.ComplainToUser("This MIDI file is broken. Try opening it in MidiEditor (https://meme.institute/midieditor) and saving it back out again.")
+            raise complain.ComplainToUser("This MIDI file is broken. Try opening it in MidiEditor (https://meme.institute/midieditor) and saving it back out again.")   
 
-    
+    def note_to_freq(self, notenum):
+        # see https://en.wikipedia.org/wiki/MIDI_Tuning_Standard
+        return (2.0 ** ((notenum - 69) / 12.0)) * 440.0
