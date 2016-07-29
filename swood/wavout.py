44d83c9112722636241a4ac75087e40b614aa279
Milkey Mouse <milkeymouse@team-ivan.com>
2016-07-29 16:41:35 -0700

    Finish(?) CachedWav

--- swood/wavout.py
+++ swood/wavout.py
@@ -1,6 +1,7 @@
-from numpy import zeros, int32
+from numpy import zeros, int32, fromfile
 from . import complain
-import collections
+
+from collections import defaultdict
 import wave
 
 
@@ -19,23 +20,29 @@ class UncachedWavFile:  # basic huge array to file
                 start:start + length] += data[chan][:length].astype(self.channels.dtype)
 
     def save(self):
-        # we can't use a with statement with wave.open here
-        # because wave.open throws a really weird error I can't catch
         try:
+            # open the file manually to catch I/O exceptions
+            # as IOError instead of wave.Error
             with (open(self.filename, "wb") if isinstance(self.filename, str) else self.filename) as wavfile:
-                with wave.Wave_write(self) as wav:
-                    wav.initfp(wavfile)
+                with wave.open(wavfile, "w") as wav:
                     nframes = len(data) // (self._sampwidth * self._nchannels)
                     wav.setparams((self.channels.shape[0],  # channels
                                    self.channels.dtype.itemsize,  # sample width
                                    self.framerate,  # sample rate
                                    self.channels.shape[1],  # number of frames
                                    "NONE", "not compressed"))  # compression type (none are supported)
-                    wav.writeframesraw(self.channels.reshape(self.channels.size,
-                                                             order="F"))
+                    wav.writeframesraw(self.channels.flatten(order="F"))
         except IOError:
             raise complain.ComplainToUser(
                 "Can't save output file '{}'.".format(self.filename))
+
+
+class defaultdictkey(defaultdict):
+    """Variation of collections.defaultdict that passes the key into the factory."""
+
+    def __missing__(self, key):
+        self[key] = self.default_factory(self, key)
+        return self[key]
 
 
 class CachedWavFile:
@@ -51,7 +58,7 @@ class CachedWavFile:
         self.chunkspacing = self.channels * self.chunksize * self.dtype.itemsize
 
         self.saved_to_disk = set()
-        self.chunks = collections.defaultdict(self.create_chunk)
+        self.chunks = defaultdictkey(self.create_chunk)
 
         if isinstance(filename, str):
             self.wavfile = open(filename, "wb+")
@@ -67,16 +74,22 @@ class CachedWavFile:
         wav._write_header(0)  # start at 0 length and patch header later
         self._header_length = self.wavfile.tell()
 
-    def create_chunk(self):
-        # todo: check self.saved_to_disk and use np.fromfile to load
-        return zeros((self.channels, self.chunksize), dtype=self.dtype)
+    def create_chunk(self, key):
+        if key in self.saved_to_disk:
+            return self.load_chunk(idx)
+        else:
+            return zeros((self.channels, self.chunksize), dtype=self.dtype)
+
+    def load_chunk(self, idx):
+        del self.saved_to_disk[idx]
+        self.wavfile.seek(self._header_length + (self.chunkspacing * idx))
+        return fromfile(self.wavfile, dtype=self.dtype, count=self.chunkspacing).reshape((self.channels, self.chunksize), order="F")
 
     def save_chunk(self, idx):
         self.wavfile.seek(self._header_length + (self.chunkspacing * idx))
-        chunk = self.chunks[idx]
-        chunk.reshape(chunk.size, order="F").tofile(self.wavfile)
-        del self.chunks[idx]
+        self.chunks[idx].flatten(order="F").tofile(self.wavfile)
         self.saved_to_disk.add(idx)
+        del self.chunks[idx]
 
     def flush_cache(self, to_idx=None):
         # we should still sort the keys even though it's theoretically not needed
@@ -111,5 +124,7 @@ class CachedWavFile:
 
     def save(self):
         self.flush_cache()
+        self.wav._datawritten = max(self.saved_to_disk)
+        self.wav._patchheader()
         if self._auto_close:
             self.wavfile.close()
