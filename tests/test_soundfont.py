a80f130373e99ee7daf040c62d9cbfb8f5be266b
Milkey Mouse <milkeymouse@team-ivan.com>
2016-08-03 21:47:54 -0700

    New test for soundfonts

---
+++ tests/test_soundfont.py
@@ -0,0 +1,17 @@
+import time
+import sys
+import os
+
+sys.path.insert(0, os.path.realpath(".."))
+import swood.soundfont
+import swood.complain
+assert(os.path.realpath(swood.soundfont.__file__) ==
+       os.path.realpath("../swood/soundfont.py"))
+
+
+print("~~~~~~~~~~ Testing soundfonts ~~~~~~~~~~")
+start = time.perf_counter()
+with swood.complain.ComplaintFormatter():
+    swood.soundfont.SoundFont("samples/test.swood")
+print("Finished soundfonts in {} seconds.".format(
+    round(time.perf_counter() - start, 2)))
