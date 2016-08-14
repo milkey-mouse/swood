9f0ec7b93c7477734fe2aacc3f02e6f9cd0007f9
Milkey Mouse <milkeymouse@team-ivan.com>
2016-08-13 21:40:36 -0700

    Revert "that was a really stupid mistake"
    This reverts commit c2d7e5c9a94593ace12dd1b9d2c5e4fdded6606d.

--- swood/__init__.py
+++ swood/__init__.py
@@ -39,7 +39,7 @@ def run_cmd(argv=sys.argv[1:]):
 
     parser.add_argument("infile", type=argparse.FileType("rb"),
                         help="a short wav file to sample as the instrument, or a swood config file")
-    parser.add_argument("midi", type=argparse.FileType("rb"),
+    parser.add_argument("midi", type=mido.MidiFile,
                         help="the MIDI to play with the wav sample")
     parser.add_argument("output", type=argparse.FileType("wb"),
                         help="path for the output wav file")
