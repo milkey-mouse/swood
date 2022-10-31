adeb68a09cab6eab20d837449518decb85ee5848
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-31 22:13:44 +0000

    Adding tarfile member sanitization to extractall()

--- pkg/win/build.py
+++ pkg/win/build.py
@@ -148,7 +148,26 @@ for bitness in (32, 64):
     if os.path.isfile(simd_filename):
         print("Using Pillow-SIMD")
         with tarfile.open(simd_filename) as pkg_tar:
-            pkg_tar.extractall("pynsist_pkgs")
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(pkg_tar, "pynsist_pkgs")
     else:
         print("Using non-accelerated Pillow. Run build_pillow-simd.py first for better performance.")
         wheels.append("Pillow==3.3.1")
